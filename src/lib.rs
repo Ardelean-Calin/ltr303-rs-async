29b412e2f634652ea83bab2159c50694d823bb32
Ardelean Calin <ardelean.calin@proton.me>
2022-09-29 20:53:11 +0300

    Implemented basic measurement flow.

--- src/lib.rs
+++ src/lib.rs
@@ -105,6 +105,48 @@ where
             .map_err(LTR303Error::I2c)
     }
 
+    pub async fn measure(&mut self, delay: &mut impl DelayUs) -> Result<u16, LTR303Error<E>> {
+        // Start measurement. Default values.
+        let command: u8 = 0b000000001;
+        self.write_register(Register::ALS_CONTR, command).await?;
+
+        delay
+            .delay_ms(100)
+            .await
+            .map_err(|_| LTR303Error::TimingError)?;
+
+        // Wait for data to be ready.
+        loop {
+            let status = self.read_register(Register::ALS_STATUS).await?;
+            // info!("Read status register: {}", status);
+            if status & 0x04 != 0 {
+                break;
+            }
+            delay
+                .delay_ms(10)
+                .await
+                .map_err(|_| LTR303Error::TimingError)?;
+        }
+
+        let ch1_0 = self.read_register(Register::ALS_DATA_CH1_LOW).await? as u16;
+        let ch1_1 = self.read_register(Register::ALS_DATA_CH1_HIGH).await? as u16;
+        let ch0_0 = self.read_register(Register::ALS_DATA_CH0_LOW).await? as u16;
+        let ch0_1 = self.read_register(Register::ALS_DATA_CH0_HIGH).await? as u16;
+        // Go to sleep.
+        let command: u8 = 0b000000000;
+        self.write_register(Register::ALS_CONTR, command).await?;
+
+        let ch0 = (ch0_1 << 8) + ch0_0;
+        let ch1 = (ch1_1 << 8) + ch1_0;
+
+        info!("Ch0: {} Ch1: {}", ch0, ch1);
+
+        Ok(raw_to_lux(ch0, ch1))
+    }
+
+    pub async fn sleep(&mut self) {}
+    pub async fn wakeup(&mut self) {}
+
     /// Gets the manufacturer ID and part ID. These should be 0x05 and 0xA0.
     pub async fn get_identifier(&mut self) -> Result<Identifier, LTR303Error<E>> {
         let mfc_id = self.read_register(Register::MANUFAC_ID).await?;
@@ -112,4 +154,29 @@ where
 
         Ok(Identifier { mfc_id, part_id })
     }
+}
+
+#[inline]
+fn raw_to_lux(lux_raw_ch0: u16, lux_raw_ch1: u16) -> u16 {
+    let ratio = lux_raw_ch1 as f32 / (lux_raw_ch0 as f32 + lux_raw_ch1 as f32);
+    let als_gain: f32 = 1.0;
+    let int_time: f32 = 1.0;
+
+    let result = if ratio < 0.45 {
+        ((1.7743 * f32::from(lux_raw_ch0)) + (1.1059 * f32::from(lux_raw_ch1)))
+            / als_gain
+            / int_time
+    } else if (0.45..0.64).contains(&ratio) {
+        ((4.2785 * f32::from(lux_raw_ch0)) - (1.9548 * f32::from(lux_raw_ch1)))
+            / als_gain
+            / int_time
+    } else if (0.64..0.85).contains(&ratio) {
+        ((0.5926 * f32::from(lux_raw_ch0)) - (0.1185 * f32::from(lux_raw_ch1)))
+            / als_gain
+            / int_time
+    } else {
+        0.0
+    };
+
+    result as u16
 }
